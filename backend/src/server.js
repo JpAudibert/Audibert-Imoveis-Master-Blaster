9a389ff00efbc2c76cbaa07c2cd3ae78be512a07
João Pedro Basso Audibert <hotsjoao@gmail.com>
2019-08-25 01:37:33 -0300

    Iniciando o repositório

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { config } = require('dotenv');
+const { join } = require('path');
+const { ok } = require('assert');
+
+const app = express();
+
+const env = process.env.NODE_ENV || 'dev';
+ok(env === 'prod' || env === 'dev', 'A env é inválida, ou dev ou prod');
+
+const configPath = join(__dirname, './../config', `.env.${env}`);
+config({
+  path: configPath,
+});
+
+const { NODE_PORT } = process.env;
+const { NODE_HOST } = process.env;
+
+console.log('PORT', NODE_PORT);
+console.log('HOST', NODE_HOST);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(NODE_PORT, NODE_HOST);
